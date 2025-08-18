4b5591c36ef4f82f969a463d45f88f6ad6142f2a
PatrickBurinR <patrick.burin@imbot.com.br>
2025-08-18 17:47:11 -0300

    Modelagem do banco de dados, NinjaModel e MissoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,5 +1,6 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
 
 // o Entity transforma uma classe em uma entidade do DB
@@ -14,6 +15,10 @@ public class NinjaModel {
   private String nome;
   private String email;
   private int idade;
+  // @ManyToOne =  muitos ninjas para uma missão
+  @ManyToOne
+  @JoinColumn(name = "missoes_id") //isso é a chave estrangeira
+  private MissoesModel missoes;
 
   public NinjaModel() {
   }
